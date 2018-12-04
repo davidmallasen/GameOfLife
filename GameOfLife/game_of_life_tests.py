24f38774d094b51346153df56e3884e164054639
DavidMallasen <dmallase@ucm.es>
2018-12-04 21:29:02 +0100

    Added tests for next state

---
+++ GameOfLife/game_of_life_tests.py
@@ -0,0 +1,115 @@
+import unittest
+from GameOfLife.game_of_life import next_board_state
+
+
+class TestGameOfLife(unittest.TestCase):
+
+    def test_dead_cells_not_3_neighbors(self):
+        init_state1 = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
+        init_state2 = [
+            [0, 1, 0],
+            [1, 0, 1],
+            [0, 1, 0]
+        ]
+        expected_next_state1 = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
+        expected_next_state2 = [
+            [0, 1, 0],
+            [1, 0, 1],
+            [0, 1, 0]
+        ]
+        actual_next_state1 = next_board_state(init_state1)
+        actual_next_state2 = next_board_state(init_state2)
+        self.assertEqual(expected_next_state1, actual_next_state1)
+        self.assertEqual(expected_next_state2, actual_next_state2)
+
+    def test_dead_cells_3_neighbors(self):
+        init_state = [
+            [0, 0, 1],
+            [0, 1, 1],
+            [0, 0, 0]
+        ]
+        expected_next_state = [
+            [0, 1, 1],
+            [0, 1, 1],
+            [0, 0, 0]
+        ]
+        actual_next_state = next_board_state(init_state)
+        self.assertEqual(expected_next_state, actual_next_state)
+
+    def test_live_cells_few_neighbors(self):
+        init_state1 = [
+            [1, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
+        init_state2 = [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 0]
+        ]
+        expected_next_state1 = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
+        expected_next_state2 = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ]
+        actual_next_state1 = next_board_state(init_state1)
+        actual_next_state2 = next_board_state(init_state2)
+        self.assertEqual(expected_next_state1, actual_next_state1)
+        self.assertEqual(expected_next_state2, actual_next_state2)
+
+    def test_live_cells_enough_neighbors(self):
+        init_state1 = [
+            [0, 1, 0],
+            [1, 0, 1],
+            [0, 1, 0]
+        ]
+        init_state2 = [
+            [1, 1, 0],
+            [1, 1, 0],
+            [0, 0, 0]
+        ]
+        expected_next_state1 = [
+            [0, 1, 0],
+            [1, 0, 1],
+            [0, 1, 0]
+        ]
+        expected_next_state2 = [
+            [0, 1, 0],
+            [1, 0, 1],
+            [0, 1, 0]
+        ]
+        actual_next_state1 = next_board_state(init_state1)
+        actual_next_state2 = next_board_state(init_state2)
+        self.assertEqual(expected_next_state1, actual_next_state1)
+        self.assertEqual(expected_next_state2, actual_next_state2)
+
+    def test_live_cells_many_neighbors(self):
+        init_state = [
+            [0, 1, 0],
+            [1, 1, 1],
+            [0, 1, 0]
+        ]
+        expected_next_state = [
+            [0, 1, 0],
+            [1, 0, 1],
+            [0, 1, 0]
+        ]
+        actual_next_state = next_board_state(init_state)
+        self.assertEqual(expected_next_state, actual_next_state)
+
+
+if __name__ == "__main__":
+    unittest.main()
