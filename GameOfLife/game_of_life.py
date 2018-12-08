610d743ca5e61a6c9774916da46012543b82f564
DavidMallasen <dmallase@ucm.es>
2018-12-08 12:06:19 +0100

    Calculates next state of the board

--- GameOfLife/game_of_life.py
+++ GameOfLife/game_of_life.py
@@ -3,30 +3,55 @@ from random import random
 DEAD = 0
 LIVE = 1
 
+"""
+Example state of height 3 and width 4 (dimensions 3x4) where the only 
+live cell has coordinates (row=2, column=3):
 
-def random_state(width=3, height=3):
+state = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 1]
+]
+"""
+
+
+def dead_state(height, width):
+    """
+    Construct a dead state with all cells set to DEAD.
+
+    :arg height: The height of the state, in cells.
+    :arg width: The width of the state, in cells.
+
+    :returns: A state of dimensions height x width, with all cells set
+        to DEAD.
+    """
+
+    return [[0 for _ in range(width)] for _ in range(height)]
+
+
+def random_state(height, width):
     """
     Construct a random state with all cells randomly set.
 
-    :arg width: the width of the state, in cells
-    :arg height: the height of the state, in cells
+    :arg height: The height of the state, in cells.
+    :arg width: The width of the state, in cells.
 
-    :returns: A state of dimensions width x height, with all cells randomly
-        set to either DEAD or LIVE with equal probability.
+    :return: A state of dimensions height x width, with all cells
+        randomly set to either DEAD or LIVE with equal probability.
     """
 
     return [[DEAD if random() >= 0.5 else LIVE
-            for _ in range(height)]
             for _ in range(width)]
+            for _ in range(height)]
 
 
 def state_height(state):
     """
-    Get the height of a state
+    Get the height of a state.
 
-    :arg state: a game state
+    :arg state: A game state.
 
-    :returns: The height of the input state
+    :return: The height of the input state.
     """
 
     return len(state)
@@ -34,31 +59,122 @@ def state_height(state):
 
 def state_width(state):
     """
-    Get the width of a state
+    Get the width of a state.
 
-    :arg state: a game state
+    :arg state: A game state.
 
-    :returns: The width of the input state
+    :return: The width of the input state.
     """
 
-    return len(state[0])
+    if len(state) == 0:
+        return 0
+    else:
+        return len(state[0])
 
 
 def render(state):
     """
-    Prints the game state
+    Prints the game state.
 
-    :arg state: a game state
+    :arg state: A game state.
     """
 
     display_as = {DEAD: ' ', LIVE: '#'}
+    width = state_width(state)
 
-    print('-' * (state_width(state) * 2 + 3))
+    print('-' * (width + 2))
     for row in state:
-        print("| %s |" % " ".join(display_as[x] for x in row))
-    print('-' * (state_width(state) * 2 + 3))
+        print("|%s|" % "".join(display_as[x] for x in row))
+    print('-' * (width + 2))
 
 
-width = 5
-height = 5
-render(random_state(width, height))
+'''
+def out_of_bounds(i, j, state):
+    """
+    Checks if a position is out of bounds.
+
+    :param i: Position row.
+    :param j: Position column.
+    :param state: A game state.
+
+    :return: True if (i, j) is out of bounds. False otherwise.
+    """
+
+    return (i < 0 or i >= state_height(state)
+            or j < 0 or j >= state_width(state))
+'''
+
+
+def number_of_neighbors(i, j, height, width, state):
+    """
+    Counts the number of LIVE neighbors of the cell in position (i, j).
+
+    :param i: Row of the cell.
+    :param j: Column of the cell.
+    :param height: Height of the input state.
+    :param width: Width of the input state.
+    :param state: A game state.
+
+    :return: Number of LIVE neighbours of the input cell in the input
+        state.
+    """
+
+    count = 0
+    for h in range(i-1, i+2):
+        if h < 0 or h >= height:
+            continue
+        for w in range(j-1, j+2):
+            if w < 0 or w >= width or (h == i and w == j):
+                continue
+            if state[h][w] == LIVE:
+                count += 1
+
+    return count
+
+
+def next_cell_value(i, j, height, width, state):
+    """
+    Calculates the next value of the cell (i, j) in the given state.
+
+    :param i: Row of the cell.
+    :param j: Column of the cell.
+    :param height: Height of the input state.
+    :param width: Width of the input state.
+    :param state: A game state
+
+    :return: Next value of the input cell in the input state.
+    """
+
+    num_neighbors = number_of_neighbors(i, j, height, width, state)
+    if state[i][j] == LIVE:
+        if num_neighbors < 2 or num_neighbors > 3:
+            return DEAD
+        else:
+            return LIVE
+    elif num_neighbors == 3:
+        return LIVE
+    else:
+        return DEAD
+
+
+def next_board_state(state):
+    """
+    Given an input state, calculates the next state of the game.
+
+    :param state: The current game state.
+    :return: The next state of the game based on the input state.
+    """
+
+    height = state_height(state)
+    width = state_width(state)
+    next_state = dead_state(height, width)
+
+    for i in range(width):
+        for j in range(height):
+            next_state[i][j] = next_cell_value(i, j, height, width, state)
+
+    return next_state
+
+
+if __name__ == "__main__":
+    render(random_state(10, 15))
